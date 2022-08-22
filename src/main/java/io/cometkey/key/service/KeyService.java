cce75829b0da044e7179e432fdb583d9b61864c6
AllyHyeseongKim <ally3k@icloud.com>
2022-08-22 13:19:58 +0900

    [feature/key] Modify the service
    src/main/java/io/cometkey/key/service/KeyService.java

--- src/main/java/io/cometkey/key/service/KeyService.java
+++ src/main/java/io/cometkey/key/service/KeyService.java
@@ -1,15 +1,22 @@
 package io.cometkey.key.service;
 
 import io.cometkey.key.domain.Key;
-import io.cometkey.key.repository.KeyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class KeyService {
 
-    private final KeyRepository keyRepository;
+    public List<Key> getKey(List<Long> idList) {
+        //TODO: worker에 해당 id들 GET 요청
+        return null;
+    }
 
-    public Key getKey(Long orderId) { return this.keyRepository.findByOrderId(orderId).get(); }
+    public Long addNewKey(Key key) {
+        //TODO: worker에 해당 key POST 요청 후 등록된 id 받아서 반환
+        return null;
+    }
 }
