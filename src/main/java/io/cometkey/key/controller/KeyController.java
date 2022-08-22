9ce9a1a845e46b05cb3741c79df87fd62113af4b
AllyHyeseongKim <ally3k@icloud.com>
2022-08-22 13:18:52 +0900

    [feature/key] Modify the controller
    src/main/java/io/cometkey/key/controller/KeyController.java

--- src/main/java/io/cometkey/key/controller/KeyController.java
+++ src/main/java/io/cometkey/key/controller/KeyController.java
@@ -1,15 +1,20 @@
 package io.cometkey.key.controller;
 
 import io.cometkey.key.domain.Key;
+import io.cometkey.key.request.KeyList;
+import io.cometkey.key.request.NewKey;
 import io.cometkey.key.response.KeyResponse;
-import io.cometkey.key.response.MifareResponse;
+import io.cometkey.key.response.UsageResponse;
 import io.cometkey.key.service.KeyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,15 +22,33 @@ public class KeyController {
 
     private final KeyService keyService;
 
-    @GetMapping("/v1/key/{order_id}")
-    public KeyResponse GetKeyInfo(@Valid @PathVariable String order_id) {
-        Key key = this.keyService.getKey(Long.valueOf(order_id));
+    @GetMapping("/v1/key")
+    public KeyResponse GetKeyInfo(@Valid @RequestBody KeyList keyList) {
 
-        //TODO: emulator 완성시 수정
+        List<UsageResponse> keyResponse = new ArrayList<>();
+        List<Key> keys = this.keyService.getKey(keyList.getIdList());
+
+        for (Key key : keys) {
+            keyResponse.add(UsageResponse.builder()
+                    .provider(key.getProvider())
+                    .isUsed(key.getIsUsed())
+                    .build()
+            );
+        }
+
         return KeyResponse.builder()
-                .key(MifareResponse.builder().
-                        mifare(key.getMifare().getMifare())
-                        .build())
+                .usageResponseList(keyResponse)
                 .build();
+    }
+
+    @PutMapping("/v1/key")
+    public Long PutKeyInfo(@Valid @RequestBody NewKey newKey) {
+
+        return this.keyService.addNewKey(Key.builder()
+                .encryptedKey(newKey.getEncryptedKey())
+                .provider(newKey.getProvider())
+                .isUsed(newKey.getIsUsed())
+                .build()
+        );
     }
 }
